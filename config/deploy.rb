bc649438f0e8c7ea00dcbf6afbdafa7a74149278
Sunki Baek <sunki.baek@gmail.com>
2015-07-26 23:05:44 +0900

    add database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,7 @@ namespace :deploy do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
   end
   after "deploy:setup", "deploy:setup_config"
 

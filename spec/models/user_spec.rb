e7b2b03956c23aec4f7d36faf1e5ee3a15ca2c78
Sunki Baek <sunki.baek@gmail.com>
2015-08-28 19:33:26 +0900

    refactor tests with shoulda matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject { build :user }
+
   context 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password_digest) }
